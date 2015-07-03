efb2098fb65d764b5830c8e0165f15c85c103962
Nikolay Galkin <mr.galkin@gmail.com>
2015-07-03 17:56:07 +0300

    acl and mongo refactoring

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.use(require('koa-validate')());
 app.use(require('./lib/response')());
 app.use(require('./lib/config')(app));
 app.use(require('./lib/jwt')(app));
-app.use(require('./lib/mongo')());
+app.use(require('./lib/mongo')(app));
 app.use(require('./lib/validation')());
-app.use(require('./lib/acl')());
+app.use(require('./lib/acl')(app));
 
 app.use(require('./app/controllers/users'));
 app.use(require('./app/controllers/auth'));
---
+++ config/permissions.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        roles: 'guest',
+        allows: [
+            {resources: ['auth/signin', 'auth/signup'], permissions: 'post'}
+        ]
+    },
+    {
+        roles: 'user',
+        allows: [
+            {resources: ['/users'], permissions: 'get'}
+        ]
+    },
+    {
+        roles: 'admin',
+        allows: [
+            {resources: ['users'], permissions: '*'}
+        ]
+    }
+];
