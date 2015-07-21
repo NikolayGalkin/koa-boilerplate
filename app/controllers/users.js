4f33cff522ddffeeec3e4ad45dbcfd9155aaa7db
Nikolay Galkin <mr.galkin@gmail.com>
2015-07-21 17:49:09 +0300

    small refactoring. Added validation

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,5 +1,6 @@
 "use strict";
 let Resource = require('koa-resource-router');
+let _        = require('lodash');
 let app      = require('../../app');
 let User     = app.db.model('User');
 
@@ -8,7 +9,16 @@ module.exports = new Resource('users', {
     this.body = yield User.find();
   }],
   create: [app.acl.can('users:create'), function*() {
-    let user = new User(this.request.body);
+    this.checkBody('email').notEmpty().isEmail('Invalid Email').trim().toLow();
+    this.checkBody('password').notEmpty().len(6, 20).trim();
+    this.checkBody('firstName').notEmpty().isAlpha().trim();
+    this.checkBody('lastName').notEmpty().isAlpha().trim();
+    if (this.errors) {
+      this.throw(422, 'Invalid params');
+    }
+
+    let params = _.pick(this.request.body, ['email', 'password', 'firstName', 'lastName', 'role']);
+    let user = new User(params);
     yield user.save();
     this.body = user;
     this.status = 201;
@@ -18,8 +28,15 @@ module.exports = new Resource('users', {
   }],
 
   update: [app.acl.can('users:update'), userParam, function*() {
-    this.request.body && delete this.request.body._id && delete this.request.body.email;
-    yield this.item.set(this.request.body).save();
+    this.checkBody('password').notEmpty().len(6, 20).trim();
+    this.checkBody('firstName').notEmpty().isAlpha().trim();
+    this.checkBody('lastName').notEmpty().isAlpha().trim();
+    if (this.errors) {
+      this.throw(422, 'Invalid params');
+    }
+
+    let params = _.pick(this.request.body, ['password', 'firstName', 'lastName', 'role']);
+    yield this.item.set(params).save();
     this.body = this.item;
   }],
   destroy: [app.acl.can('users:destroy'), userParam, function*() {
@@ -34,7 +51,8 @@ function *userParam(next) {
   if (this.errors) {
     this.throw(422, 'Invalid User id')
   }
-  this.item = yield this.db.model('User').findById(this.params.user);
+
+  this.item = yield User.findById(this.params.user);
   if (!this.item) {
     this.throw(404, 'User not found');
   }
